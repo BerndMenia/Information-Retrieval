5c3f8a5be169a945ac479b9b9a4ab9d4607af5fa
BerndMenia <bernd_menia@yahoo.de>
2019-03-21 18:55:51 +0100

    - InvertedIndex: Added a function to stem text with the PorterStemmer
    - exercise2/Main: Copied come code from exercise 1 and added code to
    test the PorterStemmer from InvertedIndex.

--- code/src/exercise2/InvertedIndex.py
+++ code/src/exercise2/InvertedIndex.py
@@ -1,11 +1,16 @@
 import nltk
 
-porter_stemmer = nltk.stem.PorterStemmer()
-text = "Hello I am a sample text =]. Please give me more input, I need it! Banana "
+class InvertedIndex:
 
-print(text)
-text = porter_stemmer.stem(text)
-print(text)
+    def __init__(self, text=""):
+        self.porter_stemmer = nltk.stem.PorterStemmer()
+        text = "Such an analysis can reveal features that are not easily visible from the variations in the individual genes and can lead to a picture of expression that is more biologically transparent and accessible to interpretation"
 
-list = [porter_stemmer.stem(s) for s in text.split()]
-print(list)
+
+    def stem_string_list(self, list):
+        return [self.porter_stemmer.stem(s) for s in list]
+
+
+    # I think it's better to work with lists of strings at this point. So maybe delete this function.
+    def stem_string(self, text):
+        return [self.porter_stemmer.stem(s) for s in text.split()]
---
+++ code/src/exercise2/Main.py
@@ -0,0 +1,25 @@
+from code.src.exercise1.Tokenizer import Tokenizer
+from code.src.exercise2.InvertedIndex import InvertedIndex
+
+
+# text = "Such an analysis can reveal features that are not easily visible from the variations in the individual genes and can lead to a picture of expression that is more biologically transparent and accessible to interpretation"
+
+
+list_file_paths = ["books/the_raven.txt"]
+
+for file_path in list_file_paths:
+    tokenizer = Tokenizer()
+    string = tokenizer.read_in(file_path)
+    tokens = tokenizer.tokenize_text(string)
+    text = tokenizer.remove_punctuation(tokens)
+    frequencies = tokenizer.get_frequency(text)
+
+    print("Number of terms:", len(text))
+    print("Number of unique terms:", len(set(text)))
+    print("NLTK stopwords:", tokenizer.get_stopwords_list_length())
+    print("Number of stopwords in text:", tokenizer.count_stopwords(text))
+    tokenizer.print_frequency(frequencies, 50)
+
+    inverted_index = InvertedIndex(text)
+    stemmed_text = inverted_index.stem_string_list(text)
+    print(stemmed_text)
