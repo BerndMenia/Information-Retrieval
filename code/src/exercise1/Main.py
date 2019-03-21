93dc65488f181f8d787f507f9f68ddacdc0d1b02
BerndMenia <bernd_menia@yahoo.de>
2019-03-21 17:50:48 +0100

    - Finished rewriting the old code and transformed it into a class.
    - Renamed tectcorpusstatistics_julia to Tokenizer.
    
    - Created Main.py from which the functions of Tokenizer get called.

---
+++ code/src/exercise1/Main.py
@@ -0,0 +1,65 @@
+from code.src.exercise1.Tokenizer import Tokenizer
+
+tokenizer = Tokenizer()
+file_path = "books/the_raven.txt"
+string = tokenizer.read_in(file_path)
+
+
+
+
+
+'''
+
+    dg_raw = read_in("books/the_raven.txt")
+    print("dg_raw", type(dg_raw), "\n")
+
+    # load data
+    dg_url = "https://www.gutenberg.org/cache/epub/174/pg174.txt"
+    #  dg_raw = urlopen(dg_url).read().decode('utf-8')
+    # dg_raw = read_in("books/the_raven.txt")
+
+    # tokenization: break up string into words an punctuation
+    dg_tokens = nltk.word_tokenize(dg_raw)
+    print(dg_tokens)
+    print("dg_tokens", type(dg_tokens), "\n")
+
+    # dg_text = nltk.Text(dg_tokens)
+    # print("dg_text", type(dg_text), "\n")
+
+    # remove punktuation and lowercase tokens
+    # I would lower case everything directly at the start when reading in the file.
+    dg_prep_tokens = [word.lower() for word in dg_tokens if word.isalpha()]
+    print(dg_prep_tokens)
+    print("dg_prep_tokens", type(dg_prep_tokens), "\n")
+
+    dg_text = nltk.Text(dg_prep_tokens)
+    print(dg_text)
+    print("dg_text", type(dg_text), "\n")
+
+
+    # total number of terms
+    dg_num_terms = str(len(dg_text))
+    print("Total number of terms: "+dg_num_terms)
+
+    # total number of unique terms
+    dg_num_unique_terms = str(len(set(dg_text)))
+    print("Number of unique terms: "+dg_num_unique_terms)
+
+    # frequency of each term
+    dg_term_freq = nltk.FreqDist(dg_text)
+    #print("Total term frequencies: \n")
+    #for word, frequency in dg_term_freq.most_common():
+    #    print(u'{};{}'.format(word, frequency))
+
+    # list of the top-50 terms with the according frequency and rank over the whole text corpus
+    #print("50 most common tokens: \n")
+    #for word, frequency in dg_term_freq.most_common(50):
+    #    print(u'{};{}'.format(word, frequency))
+    #dg_term_freq.plot(50, cumulative=True, title="50 most common tokens")
+
+    # number of stopwords contained
+    dg_num_stopwords = str(len(set(stopwords.words('english'))))
+    print("Number of stopwords: "+dg_num_stopwords)
+    print("Stopwords in text:", count_stopwords(dg_text))
+
+'''
---
+++ code/src/exercise1/Tokenizer.py
@@ -0,0 +1,64 @@
+import nltk
+from nltk.corpus import stopwords
+from urllib.request import urlopen
+from os.path import abspath
+
+
+class Tokenizer:
+
+    def __init__(self):
+        print("Hello!")
+
+
+    # Read in a text file from a local directory. Right now the books-directory  is placed in root.
+    def read_in(self, file_path):
+        file_path = abspath("../../../" + file_path)
+        file = open(file_path, "r")
+        return file.read().lower()
+
+
+    # Load a text file from a URL.
+    def load_data(self, url):
+        return urlopen(url).read().decode('utf-8')
+
+
+    # Split a given string up into tokens with NLTK
+    def tokenize_text(self, string):
+        tokens = nltk.word_tokenize(string)
+        return nltk.Text(tokens)
+
+
+    # Remove all punctuations from tokens.
+    # Shouldn't we first remove punctuation and then tokenize it instead of the other way round like we do now?
+    def remove_punctuation(self, tokens):
+        prep_tokens = [word.lower() for word in tokens if word.isalpha()]
+        return nltk.Text(prep_tokens)
+
+
+    # For each word in the string count how many times it occurs in the whole string.
+    def get_frequency(self, text):
+        return nltk.FreqDist(text)
+
+
+    # Nicely formatted frequency distribution output for the n most common words.
+    def print_frequency(self, text, n=0):
+        frequencies = nltk.FreqDist(text)
+
+        if n == 0:
+            n = len(frequencies)
+
+        for word, frequency in frequencies.most_common(n):
+            print(u'{};{}'.format(word, frequency))
+
+
+    # Check the size of the stopword list imported from NLTK.
+    def get_stopwords_list_length(self):
+        len(set(stopwords.words('english')))
+
+
+    def count_stopwords(self, text):
+        count = 0
+        for s in text:
+            if s in stopwords.words('english'):
+                count += 1
+        return count
